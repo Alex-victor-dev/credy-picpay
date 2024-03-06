878f7c6b1f538515f3ea449ddfa564c36b544121
Alex-victor-dev <alexvictoribeiro@gmail.com>
2024-03-06 12:00:06 -0300

    Init commit

---
+++ src/main/java/wakanda/academy/pay/picpaycredy/PicpayCredyApplication.java
@@ -0,0 +1,13 @@
+package wakanda.academy.pay.picpaycredy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayCredyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayCredyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wakanda/academy/pay/picpaycredy/PicpayCredyApplicationTests.java
@@ -0,0 +1,13 @@
+package wakanda.academy.pay.picpaycredy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayCredyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
